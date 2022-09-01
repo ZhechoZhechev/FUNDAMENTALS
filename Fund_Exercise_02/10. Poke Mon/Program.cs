9d6df07dfccfd7420d6f94b40df94645dee0e68d
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 13:37:48 +0300

    Arrays

---
+++ Fund_Exercise_02/10. Poke Mon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distBetweenTargets = int.Parse(Console.ReadLine());
+            int esxhaustionFactor = int.Parse(Console.ReadLine());
+            int targetsCount = 0;
+            int pokePowerCopy = pokePower;
+
+            while (pokePower >= distBetweenTargets)
+            {
+                pokePower -= distBetweenTargets;
+                targetsCount++;
+                if (pokePower * 2 == pokePowerCopy && esxhaustionFactor > 0)
+                {
+                    pokePower /= esxhaustionFactor;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
