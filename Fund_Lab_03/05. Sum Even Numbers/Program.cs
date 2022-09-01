e2c91ec3ab081f32b4683913439e979bd24c9beb
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 13:39:09 +0300

    Methods

---
+++ Fund_Lab_03/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+            }
+            Console.WriteLine(evenSum);
+        }
+    }
+}
