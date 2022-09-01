342f585d214e7e98b5393cf03eb559b019e76e06
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 11:09:50 +0300

    Data Types and Variables

---
+++ Fund_Exercise_01/04. Print and sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = numOne; i <= numTwo; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: { sum}");
+        }
+    }
+}
