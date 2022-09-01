4f6bada4c42b6be82b6d93c117ee1448990c3d2a
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 11:28:04 +0300

    Arrays

---
+++ Fund_Lab_02/09. Chars to String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    class Program
+    {
+        static void Main()
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            char three = char.Parse(Console.ReadLine());
+
+            string accumulate = "" + one + two + three;
+
+            Console.WriteLine(accumulate);
+        }
+    }
+}
---
+++ Fund_Lab_02/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double lenght = double.Parse(Console.ReadLine());
+            Console.Write("Length: ");
+            
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            
+            double volume  = (lenght * width * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}
