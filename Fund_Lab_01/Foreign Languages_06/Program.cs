680ded64e9cf8727bbbdd140967e5060d20a7764
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 11:08:38 +0300

    Data Types and Variables

---
+++ Fund_Lab_01/Foreign Languages_06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Foreign_Languages_06
+{
+    class Program
+    {
+        static void Main()
+        {
+            string countryName = Console.ReadLine();
+
+            switch (countryName)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
---
+++ Fund_Lab_01/Multiplication Table 2.0_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiplication_Table_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{num} X {multiplier} = {num * multiplier}");
+                multiplier++;
+
+            } while (multiplier <= 10);
+
+        }
+    }
+}
