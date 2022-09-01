4e841481addada3eac9333a2552edfae4714e2cf
ZhechoZhechev <mishomishev024@gmail.com>
2022-09-01 13:41:48 +0300

    List

---
+++ Fund_Lab_04/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main()
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(CalculatingPower(@base, power));
+        }
+
+        static double CalculatingPower(double @base, double power) 
+        {
+            double result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= @base;
+            }
+            return result;
+        }
+    }
+}
---
+++ Fund_Lab_04/RandomNumbersToto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandomNumbersToto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howManyNums = int.Parse(Console.ReadLine());
+            int ofHowManyNums = int.Parse(Console.ReadLine());
+        }
+
+
+    }
+}
